37654fb4cfa47b3af54d384e12a6086ec92bc38c
Aakash Raj <aakashraj914@gmail.com>
2024-05-07 14:50:54 +0530

    1. added a redux slice for infinte scroll. 2. Added the infinite scroll to the job card

--- src/components/JobCards.jsx
+++ src/components/JobCards.jsx
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { filters, companyName } from "../redux_store/slices/jobsApiSlice";
 import {
   fetchJobs,
+  selectHasMore,
   selectJobs,
   selectLoading,
-  filters,
-  companyName,
-} from "../redux_store/slices/jobsApiSlice";
+  selectIsInitialCall,
+  selectOffset,
+  changeOffset,
+} from "../redux_store/slices/infiniteScrollerSlice";
 import {
   Grid,
   Card,
@@ -67,12 +70,51 @@ const JobCards = () => {
 
   const fetchedJobs = useSelector(selectJobs);
   const isLoading = useSelector(selectLoading);
+  const offset = useSelector(selectOffset);
+  const isInitialCall = useSelector(selectIsInitialCall);
+  const hasMore = useSelector(selectHasMore);
   const selectedfilters = useSelector(filters);
   const selectedCompany = useSelector(companyName);
 
+  const loadingRef = useRef(null);
+
+  const [loadedJobs, setLoadedJobs] = useState({});
+  const [filteredJobs, setFilteredJobs] = useState([]);
+  const handleImageLoad = (index) => {
+    setLoadedJobs((prev) => ({ ...prev, [index]: true }));
+  };
+  console.log({ offset });
   useEffect(() => {
-    dispatch(fetchJobs({ limit: 10, offset: 0 }));
-  }, []);
+    console.log("called1");
+    if (hasMore) {
+      dispatch(fetchJobs({ offset: offset, limit: 10 }));
+    }
+  }, [hasMore, offset, dispatch]);
+
+  useEffect(() => {
+    console.log("called2");
+    console.log({ offset });
+    if (!loadingRef.current) return;
+
+    const loading = loadingRef.current;
+    const loadingObserver = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && !isInitialCall) {
+          dispatch(changeOffset());
+        }
+      },
+      {
+        threshold: 1,
+      }
+    );
+
+    loadingObserver.observe(loading);
+    return () => {
+      if (loading) loadingObserver.unobserve(loading);
+    };
+  }, [fetchedJobs]);
+
+  console.log({ hasMore });
 
   const handleSalaryNull = (minSalary, maxSalary) => {
     if (minSalary === null && maxSalary === null) {
@@ -106,216 +148,239 @@ const JobCards = () => {
     min_base_salary: min_base_salary,
   };
 
-   const filteredJobs =
-     !isLoading &&
-     fetchedJobs.jdList &&
-     fetchedJobs.jdList.filter((job) => {
-       if (Object.keys(selectedfilters).length > 0) {
-         if (
-           filtersLowerCase.min_experience !== null &&
-           job.minExp < parseInt(selectedfilters.min_experience)
-         ) {
-           return false;
-         }
+console.log({selectedfilters}, {selectedCompany});
 
-         if (
-           filtersLowerCase.role &&
-           filtersLowerCase.role.length > 0 &&
-           !filtersLowerCase.role.includes(job.jobRole)
-         ) {
-           return false;
-         }
+  useEffect(() => {
+    // Filter the fetchedJobs based on selected filters
+    const filteredData =
+      !isLoading &&
+      fetchedJobs &&
+      fetchedJobs.filter((job) => {
+        if (Object.keys(selectedfilters).length > 0) {
+          if (
+            filtersLowerCase.min_experience !== null &&
+            job.minExp < parseInt(selectedfilters.min_experience)
+          ) {
+            return false;
+          }
 
-         if (
-           filtersLowerCase.location &&
-           filtersLowerCase.location.length > 0 &&
-           !filtersLowerCase.location.includes(job.location)
-         ) {
-           return false;
-         }
+          if (
+            filtersLowerCase.role &&
+            filtersLowerCase.role.length > 0 &&
+            !filtersLowerCase.role.includes(job.jobRole)
+          ) {
+            return false;
+          }
 
-         if (
-           filtersLowerCase.min_base_salary &&
-           filtersLowerCase.min_base_salary !== null &&
-           job.minJdSalary < filtersLowerCase.min_base_salary
-         ) {
-           return false;
-         }
+          if (
+            filtersLowerCase.location &&
+            filtersLowerCase.location.length > 0 &&
+            !filtersLowerCase.location.includes(job.location)
+          ) {
+            return false;
+          }
 
-         if (
-           selectedCompany !== "" &&
-           !job.companyName
-             .toLowerCase()
-             .includes(selectedCompany.toLowerCase())
-         ) {
-           return false;
-         }
+          if (
+            filtersLowerCase.min_base_salary &&
+            filtersLowerCase.min_base_salary !== null &&
+            job.minJdSalary < filtersLowerCase.min_base_salary
+          ) {
+            return false;
+          }
 
-         return true; // Return true for jobs that pass all filters
-       } else {
-         return true; // No filters selected, include all jobs
-       }
-     });
+          if (
+            !job.companyName
+              .toLowerCase()
+              .includes(selectedCompany.toLowerCase())
+          ) {
+            return false;
+          }
 
-  return isLoading ? (
-    <h1>API Loading</h1>
-  ) : (
-    <>
+          return true; // Return true for jobs that pass all filters
+        } else {
+          return true; // No filters selected, include all jobs
+        }
+      });
+    setFilteredJobs(filteredData);
+  }, [fetchedJobs, selectedfilters, selectedCompany]);
+
+
+  console.log({ fetchedJobs });
+
+  return (
+    <Box>
       <Grid container columnSpacing={10} rowSpacing={5}>
-        {filteredJobs.map((job, index) => (
-          <Grid item xs={12} sm={6} lg={4} key={index}>
-            <Card
-              className="card-animation"
-              elevation={3}
-              sx={{
-                borderRadius: "1.5rem",
-                p: { sm: "0.5rem", xl: "1rem 2rem" },
-                pb: { xs: "1rem", sm: "auto" },
-                transition: "transform 0.3s ease",
-                "&:hover": {
-                  transform: "scale(1.05)",
-                },
-              }}
+        {filteredJobs &&
+          filteredJobs.map((job, index) => (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              lg={4}
+              key={index}
+              sx={loadedJobs[index] ? { opacity: 1 } : { opacity: 0 }}
+              onLoad={() => handleImageLoad(index)}
             >
-              <Box style={{ display: "flex", padding: "20px", gap: "10px" }}>
-                <img
-                  style={{ width: "35px", height: "35px" }}
-                  src={job.logoUrl}
-                  alt={`${job.companyName} logo`}
+              <Card
+                className="card-animation"
+                elevation={3}
+                sx={{
+                  borderRadius: "1.5rem",
+                  p: { sm: "0.5rem", xl: "1rem 2rem" },
+                  pb: { xs: "1rem", sm: "auto" },
+                  transition: "transform 0.3s ease",
+                  "&:hover": {
+                    transform: "scale(1.05)",
+                  },
+                }}
+              >
+                <Box style={{ display: "flex", padding: "20px", gap: "10px" }}>
+                  <img
+                    style={{ width: "35px", height: "35px" }}
+                    src={job.logoUrl}
+                    alt={`${job.companyName} logo`}
+                  />
+                  <Box>
+                    <Typography style={companyNameStyle} variant="h6">
+                      {job.companyName}
+                    </Typography>
+                    <Typography
+                      className="job-role"
+                      style={jobRoleStyle}
+                      variant="h6"
+                    >
+                      {job.jobRole}
+                    </Typography>
+                    <Typography
+                      style={jobLocationStyle}
+                      className="job-location"
+                      variant="h6"
+                    >
+                      {job.location}
+                    </Typography>
+                  </Box>
+                </Box>
+                <Typography style={salaryStyle}>
+                  Estimated Salary:{" "}
+                  {handleSalaryNull(job.minJdSalary, job.maxJdSalary)}
+                  <CheckBoxIcon color="success" />
+                </Typography>
+                <CardContent
+                  sx={{
+                    maxHeight: "250px",
+                    height: "100%",
+                    overflow: "hidden",
+                    maskImage:
+                      "linear-gradient(rgb(255, 255, 255), rgb(255, 255, 255), rgba(255, 255, 255, 0))",
+                  }}
+                >
+                  <Typography
+                    style={{ fontWeight: 500, fontSize: "20px" }}
+                    varinat="h6"
+                  >
+                    About Company:
+                  </Typography>
+                  <Typography
+                    sx={{ fontWeight: 300, fontSize: "15px" }}
+                    varinat="body1"
+                  >
+                    {job.jobDetailsFromCompany}
+                  </Typography>
+                </CardContent>
+                <JobDialougeBox
+                  jobDescription={job.jobDetailsFromCompany}
+                  jdLink={job.jdLink}
                 />
-                <Box>
-                  <Typography style={companyNameStyle} variant="h6">
-                    {job.companyName}
-                  </Typography>
-                  <Typography
-                    className="job-role"
-                    style={jobRoleStyle}
-                    variant="h6"
-                  >
-                    {job.jobRole}
-                  </Typography>
-                  <Typography
-                    style={jobLocationStyle}
-                    className="job-location"
-                    variant="h6"
-                  >
-                    {job.location}
-                  </Typography>
-                </Box>
-              </Box>
-              <Typography style={salaryStyle}>
-                Estimated Salary:{" "}
-                {handleSalaryNull(job.minJdSalary, job.maxJdSalary)}
-                <CheckBoxIcon color="success" />
-              </Typography>
-              <CardContent
-                sx={{
-                  maxHeight: "250px",
-                  height: "100%",
-                  overflow: "hidden",
-                  maskImage:
-                    "linear-gradient(rgb(255, 255, 255), rgb(255, 255, 255), rgba(255, 255, 255, 0))",
-                }}
-              >
-                <Typography
-                  style={{ fontWeight: 500, fontSize: "20px" }}
-                  varinat="h6"
+                <CardActions
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "15px",
+                    alignItems: "start",
+                    marginLeft: "0",
+                    padding: "0 18px",
+                  }}
                 >
-                  About Company:
-                </Typography>
-                <Typography
-                  sx={{ fontWeight: 300, fontSize: "15px" }}
-                  varinat="body1"
-                >
-                  {job.jobDetailsFromCompany}
-                </Typography>
-              </CardContent>
-              <JobDialougeBox
-                jobDescription={job.jobDetailsFromCompany}
-                jdLink={job.jdLink}
-              />
-              <CardActions
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "15px",
-                  alignItems: "start",
-                  marginLeft: "0",
-                  padding: "0 18px",
-                }}
-              >
-                <Box sx={{ margin: "15px 0" }}>
-                  <Typography
-                    sx={{
-                      textAlign: "start",
-                      color: "#8b8b8b",
-                      fontSize: "15px",
-                      fontWeight: 500,
-                      letterSpacing: "1px",
-                    }}
+                  <Box sx={{ margin: "15px 0" }}>
+                    <Typography
+                      sx={{
+                        textAlign: "start",
+                        color: "#8b8b8b",
+                        fontSize: "15px",
+                        fontWeight: 500,
+                        letterSpacing: "1px",
+                      }}
+                    >
+                      Minimum Experience: <br />
+                      <span style={{ color: "black" }}>
+                        {job.minExp === null
+                          ? "Not Specified"
+                          : `${job.minExp} years`}
+                      </span>
+                    </Typography>
+                  </Box>
+                  <Link
+                    href={job.jdLink}
+                    target="_blank"
+                    underline="none"
+                    sx={{ width: "100%" }}
                   >
-                    Minimum Experience: <br />
-                    <span style={{ color: "black" }}>
-                      {job.minExp === null
-                        ? "Not Specified"
-                        : `${job.minExp} years`}
-                    </span>
-                  </Typography>
-                </Box>
-                <Link
-                  href={job.jdLink}
-                  target="_blank"
-                  underline="none"
-                  sx={{ width: "100%" }}
-                >
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      sx={{
+                        display: "flex",
+                        gap: "10px",
+                        backgroundColor: "#55EFC4",
+                        color: "black",
+                        textTransform: "none",
+                        "&:hover": {
+                          backgroundColor: "#55EFC4",
+                        },
+                        py: "0.75rem",
+                      }}
+                    >
+                      <ElectricBoltIcon style={{ color: "yellow" }} /> Easy
+                      Apply
+                    </Button>
+                  </Link>
                   <Button
                     fullWidth
                     variant="contained"
                     sx={{
-                      display: "flex",
-                      gap: "10px",
-                      backgroundColor: "#55EFC4",
-                      color: "black",
+                      marginLeft: "0",
                       textTransform: "none",
-                      "&:hover": {
-                        backgroundColor: "#55EFC4",
-                      },
+                      gap: "10px",
                       py: "0.75rem",
                     }}
                   >
-                    <ElectricBoltIcon style={{ color: "yellow" }} /> Easy Apply
+                    {referralImages.map((referralImage, index) => (
+                      <Avatar
+                        sx={{
+                          width: "30px",
+                          height: "30px",
+                          filter: "blur(1.3px)",
+                        }}
+                        key={index}
+                        alt={referralImage.name}
+                        src={referralImage.img}
+                      />
+                    ))}
+                    Unlock referal asks
                   </Button>
-                </Link>
-                <Button
-                  fullWidth
-                  variant="contained"
-                  sx={{
-                    marginLeft: "0",
-                    textTransform: "none",
-                    gap: "10px",
-                    py: "0.75rem",
-                  }}
-                >
-                  {referralImages.map((referralImage, index) => (
-                    <Avatar
-                      sx={{
-                        width: "30px",
-                        height: "30px",
-                        filter: "blur(1.3px)",
-                      }}
-                      key={index}
-                      alt={referralImage.name}
-                      src={referralImage.img}
-                    />
-                  ))}
-                  Unlock referal asks
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
       </Grid>
-    </>
+      {hasMore && (
+        <div ref={loadingRef}>
+          <div className="loader">
+            <h1 className="spin"> Loading....</h1>
+          </div>
+        </div>
+      )}
+    </Box>
   );
 };
 
